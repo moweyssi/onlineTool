5b2cdac95fed72542ef9fdbf540ad4db8747f242
Maxim <maxim.oweyssi@est.org.uk>
2024-09-19 07:47:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ input_df_viz = st.data_editor(input_data, use_container_width=True)
 if st.button('Generate Predictions'):
     input_data_calc = input_df_viz
     st.text(input_data_calc)
-    input_data_calc.append(['MonthsRunning',5,6,7,8,9,10,11])
+    input_data_calc.append([5,6,7,8,9,10,11])
     st.dataframe(input_data_calc)
     # Call the prediction function
     predictions = make_predictions(input_data_calc)
