b27614d34c481bf6f86d8baa0eac4a313d7e8c2c
Maxim <maxim.oweyssi@est.org.uk>
2024-09-19 16:52:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import numpy as np
 import pickle
 import plotly.graph_objects as go
 from sklearn.ensemble import RandomForestRegressor
-
+st.set_page_config(layout="wide")
 # Load the trained RandomForest model
 with open("rfr_newmodel.pkl", "rb") as file:
     model = pickle.load(file)
